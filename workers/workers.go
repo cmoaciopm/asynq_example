eaf6cca4b03fc6208fbd28e0fc2e1da34bec239c
Anmin Gong <gonganmin@songmont.cn>
2025-05-23 15:06:16 +0800

    Add logging middleware

--- workers/workers.go
+++ workers/workers.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"github.com/hibiken/asynq"
 
 	"asynq-quickstart/task"
 )
+
+func loggingMiddleware(h asynq.Handler) asynq.Handler {
+	return asynq.HandlerFunc(func(ctx context.Context, t *asynq.Task) error {
+		start := time.Now()
+		log.Printf("Start processing %q", t.Type())
+		err := h.ProcessTask(ctx, t)
+		if err != nil {
+			return err
+		}
+		log.Printf("Finished processing %q: Elapsed Time = %v", t.Type(), time.Since(start))
+		return nil
+	})
+}
 
 func main() {
 	srv := asynq.NewServer(
@@ -15,6 +30,7 @@ func main() {
 	)
 
 	mux := asynq.NewServeMux()
+	mux.Use(loggingMiddleware)
 	mux.HandleFunc("email:welcome", task.HandleWelcomeEmailTask)
 	mux.HandleFunc("email:reminder", task.HandleReminderEmailTask)
 
